0078dc3091255a92be93c56ea2f2ed400a641687
Bhaskar K <xlinkerz@gmail.com>
2021-01-31 17:59:17 +1100

    Utility pkg

---
+++ utils/utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"os"
+	"strings"
+)
+
+func GetEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func JoinString(strs ...string) string {
+	var sb strings.Builder
+	for _, str := range strs {
+		sb.WriteString(str)
+	}
+	return sb.String()
+}
