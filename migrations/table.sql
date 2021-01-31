1baa131a63320e50f3ee99b1ea528d1bb3f25acb
Bhaskar K <xlinkerz@gmail.com>
2021-01-31 17:59:27 +1100

    Migrations

---
+++ migrations/table.sql
@@ -0,0 +1,36 @@
+
+/* DROP TABLE IF EXISTS `articles`; */
+
+CREATE TABLE IF NOT EXISTS `page` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `title` mediumtext NOT NULL,
+    `body` longtext NOT NULL,
+    `date` datetime DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/* DROP TABLE IF EXISTS `tags`; */
+
+CREATE TABLE IF NOT EXISTS `tags` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(200) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+/* DROP TABLE IF EXISTS `tag_relation`; */
+
+CREATE TABLE IF NOT EXISTS `tag_relation` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `article_id` int(11) NOT NULL,
+    `tag_id` int(11) NOT NULL,
+    `date` datetime DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY(article_id)
+        REFERENCES articles(id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (tag_id)
+        REFERENCES tags(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
