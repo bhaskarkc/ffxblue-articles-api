9d732838bc0769012284c13b3bd2f4a95e39bfed
Bhaskar K <xlinkerz@gmail.com>
2021-02-05 08:19:03 +1100

    dotenv

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.15
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/joho/godotenv v1.3.0
+	github.com/ory/dockertest/v3 v3.6.3
 	go.uber.org/zap v1.16.0
 )
--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"github.com/bhaskarkc/ffxblue-articles-api/logger"
 	"github.com/bhaskarkc/ffxblue-articles-api/server"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	if err := godotenv.Load(".env", ".env.example"); err != nil {
+		logger.Error("ENV vars not loaded", err)
+	}
+}
 
 func main() {
 	server.Start()
